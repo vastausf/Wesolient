a3a3e5019b35a955c811a54520a5b76aa3663d44
nikita <vastausf@gmail.com>
2021-01-20 17:52:13 +0300

    Add test

--- app/src/androidTest/java/com/vastausf/wesolient/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/vastausf/wesolient/ExampleInstrumentedTest.kt
@@ -6,27 +6,42 @@ import androidx.test.espresso.action.ViewActions.typeText
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
 import com.vastausf.wesolient.presentation.ui.activity.main.MainActivity
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 
 @RunWith(AndroidJUnit4::class)
+@LargeTest
 class ExampleInstrumentedTest {
+    private lateinit var testScopeTitle: String
+    private lateinit var testScopeUrl: String
+
     @get:Rule
     var activityScenarioRule = ActivityScenarioRule(
         MainActivity::class.java
     )
 
+    @Before
+    fun initializeStrings() {
+        testScopeTitle = "testScopeTitle"
+        testScopeUrl = "testScopeUrl"
+    }
+
     @Test
-    fun useAppContext() {
+    fun createNewScope() {
         onView(withId(R.id.fabCreateScope))
             .perform(click())
 
-        onView(withId(R.id.etTitle))
-            .perform(typeText("Websocket.org"))
-        onView(withId(R.id.etUrl))
-            .perform(typeText("wss://echo.websocket.org"))
+        onView(withId(R.id.etScopeTitle))
+            .perform(typeText(testScopeTitle))
+        onView(withId(R.id.etScopeUrl))
+            .perform(typeText(testScopeUrl))
+
+        onView(withId(R.id.bCreateScope))
+            .perform(click())
     }
 }
